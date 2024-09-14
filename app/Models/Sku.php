4b4b4c6d7e856d138f78e4ab10817f564d76b497
Deivison santos <gomesdeivison060@gmail.com>
2024-09-14 14:21:57 -0300

    fix: Modelagem: models

--- app/Models/Sku.php
+++ app/Models/Sku.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Sku extends Model
@@ -25,5 +26,13 @@ class Sku extends Model
     public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function features(): BelongsToMany
+    {
+        return $this->belongsToMany(Feature::class)
+            ->using(FeatureSku::class)
+            ->withPivot(['value']);
+
     }
 }
